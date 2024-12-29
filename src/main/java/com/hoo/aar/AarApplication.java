3cd3c004a5cff03d2c512f73aea2667b3a431073
SangyeopNam <spearoad15@gmail.com>
2024-12-29 09:51:10 +0900

    init backend repository

---
+++ src/main/java/com/hoo/aar/AarApplication.java
@@ -0,0 +1,13 @@
+package com.hoo.aar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AarApplication.class, args);
+	}
+
+}
