acb81d6e6cf1ccd971fe4ce7053fc0a26a088209
SangyeopNam <spearoad15@gmail.com>
2025-01-09 21:10:36 +0900

    docs : 에러코드 작성

--- src/main/java/com/hoo/aoo/admin/application/service/AdminErrorCode.java
+++ src/main/java/com/hoo/aoo/admin/application/service/AdminErrorCode.java
@@ -5,13 +5,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.*;
 
 @Getter
 @AllArgsConstructor
 public enum AdminErrorCode implements ErrorCode {
 
-    FILE_SIZE_LIMIT_EXCEED("ADMIN-HOUSE-1", BAD_REQUEST, "파일 용량 한도를 초과했습니다.");
+    PIXEL_LIMIT_EXCEED("ADMIN-HOUSE-1", BAD_REQUEST, "이미지 픽셀 크기 한도를 초과했습니다."),
+    IMAGE_SIZE_EXCEED("ADMIN-HOUSE-2", BAD_REQUEST, "이미지 크기 한도를 초과했습니다."),
+    ROOM_NAME_DUPLICATED("ADMIN-HOUSE-3", CONFLICT, "방 이름이 중복됩니다."),
+    IMAGE_ID_NOT_FOUND("ADMIN-HOUSE-4", BAD_REQUEST, "메타데이터에 등록되지 않은 이미지가 존재합니다."),
+    IMAGE_FILE_NOT_FOUND("ADMIN-HOUSE-5", BAD_REQUEST, "메타데이터에 등록된 이미지가 존재하지 않습니다.");
 
     private final String code;
     private final HttpStatus status;
--- src/main/java/com/hoo/aoo/file/application/service/FileErrorCode.java
+++ src/main/java/com/hoo/aoo/file/application/service/FileErrorCode.java
@@ -12,11 +12,12 @@ import static org.springframework.http.HttpStatus.*;
 @AllArgsConstructor
 public enum FileErrorCode implements ErrorCode {
 
-    NEW_FILE_CREATION_FAILED("FILE-PUBLIC-1", INTERNAL_SERVER_ERROR, "새로운 파일을 생성하는 과정에서 오류가 발생했습니다."),
-    FILE_SIZE_LIMIT_EXCEED("FILE-PUBLIC-2", BAD_REQUEST, "파일 용량 한도를 초과했습니다."),
-    FILE_NAME_DUPLICATION("FILE-PUBLIC-3", BAD_REQUEST, "파일 이름이 중복됩니다."),
-    RETRIEVE_FILE_FAILED("FILE-PUBLIC-4", INTERNAL_SERVER_ERROR, "파일을 불러오는 데 실패했습니다."),
-    FILE_NOT_FOUND("FILE-PUBLIC-5", NOT_FOUND, "파일을 찾을 수 없습니다.");
+    FILE_SIZE_LIMIT_EXCEED("FILE-PUBLIC-1", BAD_REQUEST, "파일 용량 한도를 초과했습니다."),
+    INVALID_FILE_EXTENSION("FILE-PUBLIC-2", BAD_REQUEST, "유효하지 않은 파일 확장자입니다."),
+    FILE_NOT_FOUND("FILE-PUBLIC-3", NOT_FOUND, "파일을 찾을 수 없습니다."),
+    FILE_NAME_DUPLICATION("FILE-PUBLIC-4", CONFLICT, "중복된 파일명입니다."),
+    RETRIEVE_FILE_FAILED("FILE-PUBLIC-5", INTERNAL_SERVER_ERROR, "파일을 불러오는 데 실패했습니다."),
+    NEW_FILE_CREATION_FAILED("FILE-PUBLIC-6", INTERNAL_SERVER_ERROR, "새로운 파일을 생성하는 데 실패했습니다.");
 
     private final String code;
     private final HttpStatus status;
