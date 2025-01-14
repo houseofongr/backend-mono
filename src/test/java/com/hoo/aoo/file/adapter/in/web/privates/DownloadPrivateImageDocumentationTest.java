13595e78a3354092b2d0fcb4bcb264812b220868
SangyeopNam <spearoad15@gmail.com>
2025-01-15 00:41:40 +0900

    test : API 문서 테스트 작성

---
+++ src/test/java/com/hoo/aoo/file/adapter/in/web/privates/DownloadPrivateImageDocumentationTest.java
@@ -0,0 +1,56 @@
+package com.hoo.aoo.file.adapter.in.web.privates;
+
+import com.hoo.aoo.common.adapter.in.web.config.AbstractDocumentationTest;
+import com.hoo.aoo.file.adapter.out.persistence.entity.FileJpaEntity;
+import com.hoo.aoo.file.adapter.out.persistence.repository.FileJpaRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.restdocs.RestDocumentationContext;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class DownloadPrivateImageDocumentationTest extends AbstractDocumentationTest {
+
+    @Autowired
+    FileJpaRepository fileJpaRepository;
+
+    @Override
+    protected String getBaseUrl() {
+        return "file.archiveofongr.site";
+    }
+
+    @Test
+    @DisplayName("이미지파일 다운로드 API")
+    void testFile() throws Exception {
+
+        ClassPathResource resource = new ClassPathResource("private/images/secret.png");
+        byte[] bytes = Files.readAllBytes(resource.getFile().toPath());
+        FileJpaEntity entity = new FileJpaEntity(null, "secret.png", "secret.png", resource.getFile().getParent(), false, (long) bytes.length, null);
+        fileJpaRepository.save(entity);
+
+        mockMvc.perform(get("/private/images/{imageId}", entity.getId()))
+                .andExpect(status().is(200))
+                .andDo(document("file-private-images-download",
+                        operation -> {
+                            var context = (RestDocumentationContext) operation.getAttributes().get(RestDocumentationContext.class.getName());
+                            var path = Paths.get(context.getOutputDirectory().getAbsolutePath(), operation.getName(), "response-file.adoc");
+                            var outputStream = new ByteArrayOutputStream();
+                            outputStream.write("++++\n".getBytes());
+                            outputStream.write("<img src=\"data:image/png;base64,".getBytes());
+                            outputStream.write(Base64.getEncoder().encode(operation.getResponse().getContent()));
+                            outputStream.write("\"/>\n".getBytes());
+                            outputStream.write("++++\n".getBytes());
+                            Files.createDirectories(path.getParent());
+                            Files.write(path, outputStream.toByteArray());
+                        }));
+    }
+}
---
+++ src/test/java/com/hoo/aoo/file/adapter/in/web/publics/DownloadPublicImageDocumentationTest.java
@@ -0,0 +1,66 @@
+package com.hoo.aoo.file.adapter.in.web.publics;
+
+import com.hoo.aoo.common.adapter.in.web.config.AbstractDocumentationTest;
+import com.hoo.aoo.common.adapter.in.web.config.DocumentationTest;
+import com.hoo.aoo.file.adapter.out.persistence.entity.FileJpaEntity;
+import com.hoo.aoo.file.adapter.out.persistence.repository.FileJpaRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.restdocs.RestDocumentationContext;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class DownloadPublicImageDocumentationTest extends AbstractDocumentationTest {
+
+    @Autowired
+    FileJpaRepository fileJpaRepository;
+
+    @Override
+    protected String getBaseUrl() {
+        return "file.archiveofongr.site";
+    }
+
+    @Test
+    @DisplayName("이미지파일 다운로드 API")
+    void testFile() throws Exception {
+
+        ClassPathResource resource = new ClassPathResource("public/images/logo.png");
+        byte[] bytes = Files.readAllBytes(resource.getFile().toPath());
+        FileJpaEntity entity = new FileJpaEntity(null, "logo.png", "logo.png", resource.getFile().getParent(), false, (long) bytes.length, null);
+        fileJpaRepository.save(entity);
+
+        mockMvc.perform(get("/public/images/{imageId}", entity.getId()))
+                .andExpect(status().is(200))
+                .andDo(document("file-public-images-download",
+                        operation -> {
+                            var context = (RestDocumentationContext) operation.getAttributes().get(RestDocumentationContext.class.getName());
+                            var path = Paths.get(context.getOutputDirectory().getAbsolutePath(), operation.getName(), "response-file.adoc");
+                            var outputStream = new ByteArrayOutputStream();
+                            outputStream.write("++++\n".getBytes());
+                            outputStream.write("<img src=\"data:image/png;base64,".getBytes());
+                            outputStream.write(Base64.getEncoder().encode(operation.getResponse().getContent()));
+                            outputStream.write("\"/>\n".getBytes());
+                            outputStream.write("++++\n".getBytes());
+                            Files.createDirectories(path.getParent());
+                            Files.write(path, outputStream.toByteArray());
+                        }));
+    }
+
+}
