bb8400db8f0494e583634f8d87bf36bbfae33d08
SangyeopNam <spearoad15@gmail.com>
2025-01-16 00:16:30 +0900

    test : 하우스 정보 수정  API 문서화 테스트 완료

--- src/main/java/com/hoo/aoo/admin/adapter/in/web/house/UpdateHouseInfoController.java
+++ src/main/java/com/hoo/aoo/admin/adapter/in/web/house/UpdateHouseInfoController.java
@@ -1,19 +1,38 @@
 package com.hoo.aoo.admin.adapter.in.web.house;
 
 import com.hoo.aoo.admin.application.port.in.HouseIdResult;
+import com.hoo.aoo.admin.application.port.in.UpdateHouseInfoCommand;
+import com.hoo.aoo.admin.application.port.in.UpdateHouseInfoUseCase;
+import com.hoo.aoo.common.adapter.in.web.MessageDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class UpdateHouseInfoController {
 
-    @PostMapping("/admin/houses/update/{houseId}")
-    ResponseEntity<HouseIdResult> update(@PathVariable Long houseId) {
-        return new ResponseEntity<>(new HouseIdResult(houseId), HttpStatus.OK);
+    private final UpdateHouseInfoUseCase updateHouseInfoUseCase;
+
+    @PatchMapping("/admin/houses/{houseId}")
+    ResponseEntity<MessageDto> update(
+            @PathVariable Long houseId,
+            @RequestBody Request request) {
+
+        UpdateHouseInfoCommand command = new UpdateHouseInfoCommand(houseId,
+                request.title(),
+                request.author(),
+                request.description());
+
+        return new ResponseEntity<>(updateHouseInfoUseCase.update(command), HttpStatus.OK);
+    }
+
+    private record Request(
+            String title,
+            String author,
+            String description
+    ) {
+
     }
 }
