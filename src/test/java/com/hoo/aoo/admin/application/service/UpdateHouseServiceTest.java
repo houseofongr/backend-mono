b9c9fc3515bc82321d5ce9132657e627db4cd720
SangyeopNam <spearoad15@gmail.com>
2025-01-16 00:59:25 +0900

    test : 룸 업데이트 서비스 통합테스트 작성

--- src/test/java/com/hoo/aoo/admin/application/service/UpdateHouseServiceTest.java
+++ src/test/java/com/hoo/aoo/admin/application/service/UpdateHouseServiceTest.java
@@ -1,8 +1,10 @@
 package com.hoo.aoo.admin.application.service;
 
 import com.hoo.aoo.admin.application.port.in.UpdateHouseInfoCommand;
+import com.hoo.aoo.admin.application.port.in.UpdateRoomInfoCommand;
 import com.hoo.aoo.admin.application.port.out.LoadHousePort;
 import com.hoo.aoo.admin.application.port.out.UpdateHousePort;
+import com.hoo.aoo.admin.application.port.out.UpdateRoomPort;
 import com.hoo.aoo.admin.domain.house.House;
 import com.hoo.aoo.admin.domain.house.HouseId;
 import com.hoo.aoo.common.adapter.in.web.MessageDto;
@@ -23,17 +25,19 @@ class UpdateHouseServiceTest {
 
     LoadHousePort loadHousePort;
     UpdateHousePort updateHousePort;
+    UpdateRoomPort updateRoomPort;
 
     @BeforeEach
     void init() {
         loadHousePort = mock();
         updateHousePort = mock();
-        sut = new UpdateHouseService(loadHousePort, updateHousePort);
+        updateRoomPort = mock();
+        sut = new UpdateHouseService(loadHousePort, updateHousePort, updateRoomPort);
     }
 
     @Test
     @DisplayName("하우스 업데이트 서비스 테스트")
-    void testUpdateRoomInfo() throws Exception {
+    void testUpdateHouseInfo() throws Exception {
         // given
         House house = getHouse(new HouseId("cozy house", "leaf", "this is cozy house"), List.of());
         UpdateHouseInfoCommand command = new UpdateHouseInfoCommand(1L, "not cozy house", "arang", "this is not cozy house.");
@@ -49,5 +53,22 @@ class UpdateHouseServiceTest {
         assertThat(message.message()).isEqualTo("1번 하우스 정보 수정이 완료되었습니다.");
     }
 
+    @Test
+    @DisplayName("룸 업데이트 서비스 테스트")
+    void testUpdateRoomInfo() throws Exception{
+        // given
+        House houseWithRoom = getHouseWithRoom();
+        UpdateRoomInfoCommand command = new UpdateRoomInfoCommand(1L, "거실", "욕실");
+
+        // when
+        when(loadHousePort.load(1L)).thenReturn(Optional.of(houseWithRoom));
+        MessageDto message = sut.update(command);
+
+        // then
+        verify(loadHousePort, times(1)).load(1L);
+        verify(updateRoomPort, times(1)).update(any(),any());
+
+        assertThat(message.message()).isEqualTo("1번 하우스 거실 룸의 정보 수정이 완료되었습니다.");
+    }
 
 }
