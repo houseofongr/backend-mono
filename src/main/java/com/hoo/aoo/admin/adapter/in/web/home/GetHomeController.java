21ed225234538b0151aaa9159b37b64e6e3ca79a
SangyeopNam <spearoad15@gmail.com>
2025-01-21 18:53:43 +0900

    docs : 홈 조회 API 문서 테스트 성공

--- src/main/java/com/hoo/aoo/admin/adapter/in/web/home/GetHomeController.java
+++ src/main/java/com/hoo/aoo/admin/adapter/in/web/home/GetHomeController.java
@@ -1,6 +1,7 @@
 package com.hoo.aoo.admin.adapter.in.web.home;
 
 import com.hoo.aoo.admin.application.port.in.home.QueryHomeResult;
+import com.hoo.aoo.admin.application.port.in.home.QueryHomeUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,21 +9,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 public class GetHomeController {
 
+    private final QueryHomeUseCase queryHomeUseCase;
+
     @GetMapping("/admin/homes/{homeId}")
     public ResponseEntity<QueryHomeResult> getHome(@PathVariable Long homeId) {
-        QueryHomeResult 거실 = new QueryHomeResult(
-                1L, "leaf의 cozy house","January.21. 2025", "January.21. 2025",
-                new QueryHomeResult.HouseInfo(5000f, 5000f, 1L),
-                List.of(new QueryHomeResult.RoomInfo(1L, "거실", 0f, 0f, 0f, 5000f, 1000f, 1L))
-        );
-
-        return new ResponseEntity<>(거실, HttpStatus.OK);
+        return new ResponseEntity<>(queryHomeUseCase.queryHome(homeId), HttpStatus.OK);
     }
 
 }
