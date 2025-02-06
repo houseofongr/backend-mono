95245a5993dc15a050454f8f2449b2c4f0edd51e
SangyeopNam <spearoad15@gmail.com>
2025-02-07 01:02:06 +0900

    choir : static frontend 경로 설정

---
+++ src/main/java/com/hoo/aoo/aar/adapter/in/web/user/DeleteMyAccountController.java
@@ -0,0 +1,4 @@
+package com.hoo.aoo.aar.adapter.in.web.user;
+
+public class DeleteMyAccountController {
+}
---
+++ src/main/java/com/hoo/aoo/admin/adapter/in/web/resource/RefreshController.java
@@ -0,0 +1,13 @@
+package com.hoo.aoo.admin.adapter.in.web.resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RefreshController {
+
+    @GetMapping({"/users/**", "/houses/**", "/soundsources/**"})
+    public String refresh() {
+        return "index.html";
+    }
+}
