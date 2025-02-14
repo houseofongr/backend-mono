24e378407d2e1e679e8e101859b56e5a029eb58c
SangyeopNam <spearoad15@gmail.com>
2025-02-14 23:36:25 +0900

    fix : local authentication fail exception logging

--- src/main/java/com/hoo/aoo/aar/adapter/in/web/authn/security/OAuth2FailureHandler.java
+++ src/main/java/com/hoo/aoo/aar/adapter/in/web/authn/security/OAuth2FailureHandler.java
@@ -16,7 +16,7 @@ public class OAuth2FailureHandler implements AuthenticationFailureHandler {
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        log.error("Authentication Failed : {}", exception.getMessage());
+        log.error("Authentication Failed : {} | {}", exception.getMessage(), exception.getStackTrace());
         response.sendError(401);
     }
 }
