22bd845ad36518f186eeec0d40b24fb6a1fc55f2
SangyeopNam <spearoad15@gmail.com>
2025-06-04 18:00:24 -0500

    docs : 카테고리 API 명세 작성

---
+++ src/main/java/com/aoo/admin/adapter/in/web/category/GetCategoryController.java
@@ -0,0 +1,11 @@
+package com.aoo.admin.adapter.in.web.category;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class GetCategoryController {
+
+
+}
